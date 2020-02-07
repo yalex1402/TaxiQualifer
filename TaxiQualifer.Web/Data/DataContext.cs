83ba0961c5e9bceaa9480dab1b12222a55d9f1c5
Alexander GL <yagarcia1402@gmail.com>
2020-02-07 17:24:45 -0500

    The begin

---
+++ TaxiQualifer.Web/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaxiQualifer.Web.Data.Entities;
+
+namespace TaxiQualifer.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<TaxiEntity> Taxis { get; set; }
+    }
+}
