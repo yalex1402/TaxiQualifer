768f7566496794e99bdea352d37f0ede872fd08d
Alexander GL <yagarcia1402@gmail.com>
2020-02-12 17:45:50 -0500

    Add Trip and TripDetail Entities

--- TaxiQualifer.Web/Data/DataContext.cs
+++ TaxiQualifer.Web/Data/DataContext.cs
@@ -9,5 +9,10 @@ namespace TaxiQualifer.Web.Data
         {
         }
         public DbSet<TaxiEntity> Taxis { get; set; }
+
+        public DbSet<TripEntity> Trips { get; set; }
+
+        public DbSet<TripDetailEntity> TripDetails { get; set; }
+
     }
 }
