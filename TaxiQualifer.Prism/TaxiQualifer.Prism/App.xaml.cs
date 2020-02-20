b6cf3d01f65589d1346cad94b9d8e00ee6f633e7
Alexander GL <yagarcia1402@gmail.com>
2020-02-20 16:19:09 -0500

    HomePage with Prism

--- TaxiQualifer.Prism/TaxiQualifer.Prism/App.xaml.cs
+++ TaxiQualifer.Prism/TaxiQualifer.Prism/App.xaml.cs
@@ -10,11 +10,7 @@ namespace TaxiQualifer.Prism
 {
     public partial class App
     {
-        /* 
-         * The Xamarin Forms XAML Previewer in Visual Studio uses System.Activator.CreateInstance.
-         * This imposes a limitation in which the App class must have a default constructor. 
-         * App(IPlatformInitializer initializer = null) cannot be handled by the Activator.
-         */
+
         public App() : this(null) { }
 
         public App(IPlatformInitializer initializer) : base(initializer) { }
@@ -23,13 +19,14 @@ namespace TaxiQualifer.Prism
         {
             InitializeComponent();
 
-            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+            await NavigationService.NavigateAsync("NavigationPage/HomePage");
         }
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
             containerRegistry.RegisterForNavigation<NavigationPage>();
-            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+
+            containerRegistry.RegisterForNavigation<HomePage, HomePageViewModel>();
         }
     }
 }
---
+++ TaxiQualifer.Prism/TaxiQualifer.Prism/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TaxiQualifer.Prism.ViewModels
+{
+    public class HomePageViewModel : ViewModelBase
+    {
+       
+        public HomePageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            Title = "Taxi Qualifier";
+        }
+    }
+}
