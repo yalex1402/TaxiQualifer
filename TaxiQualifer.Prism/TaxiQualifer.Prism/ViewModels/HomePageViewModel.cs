32359ee94a241b3765406db8ac693f7f50ebf97b
Alexander GL <yagarcia1402@gmail.com>
2020-02-20 20:57:06 -0500

    Add a library to run Maps

--- TaxiQualifer.Prism/TaxiQualifer.Prism/ViewModels/HomePageViewModel.cs
+++ TaxiQualifer.Prism/TaxiQualifer.Prism/ViewModels/HomePageViewModel.cs
@@ -1,15 +1,10 @@
-﻿using Prism.Commands;
-using Prism.Mvvm;
-using Prism.Navigation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Prism.Navigation;
 
 namespace TaxiQualifer.Prism.ViewModels
 {
     public class HomePageViewModel : ViewModelBase
     {
-       
+
         public HomePageViewModel(INavigationService navigationService) : base(navigationService)
         {
             Title = "Taxi Qualifier";
